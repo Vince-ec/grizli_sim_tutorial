2c97b5de8d237549f03c9a9ff8d0c01fb9a177bf
Vince-ec <vestrada78840@gmail.com>
2018-10-11 23:00:06 -0500

    rm files

--- gen_grizli_sim.py
+++
@@ -1,8 +0,0 @@
-from sim_tools import Gen_spec, Gen_sim
-import numpy as np
-
-wv,fl = Gen_spec(np.arange(4000,9000,0.1),[6549,6564,6586],[3,10,2],[2E-18,1E-17,3E-18])
-test = Gen_sim(redshift=0.5,wavelength=wv,flux=fl,signal_to_noise=20)
-test.Sim_data()
-
-np.save('test',[test.mwv,test.mfl,test.flx_err,test.mer])
